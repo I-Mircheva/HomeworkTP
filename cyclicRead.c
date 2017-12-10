fbe5703fdbee4189d6d954836b026af23df3b4ba
Ivona <ivona1mircheva@gmail.com>
2017-12-10 19:15:52 +0200

    Changed variable names
    Signed-off-by: Ivona <ivona1mircheva@gmail.com>

--- cyclicRead.c
+++ cyclicRead.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include "cyclicBuf.h"
 #include "gen.h"
+
 int main() {
   int shmFd = shm_open("shared_buffer", O_RDONLY, 0);
   if (shmFd == -1) {
@@ -13,23 +14,22 @@ int main() {
     return 1;
   }
 
-  struct cyclic_buf* mem = mmap(NULL, sizeof(struct cyclic_buf), PROT_READ, MAP_SHARED, shmFd, 0);
+  struct cyclic_buffer* mem = mmap(NULL, sizeof(struct cyclic_buffer), PROT_READ, MAP_SHARED, shmFd, 0);
   if (mem == NULL) {
     perror("Can't mmap");
     return -1;
   }
 
   uint64_t cur_pos = 0;
-  printf("starting at %ld\n", cur_pos);
-  while (true) {
-    while (mem->pos == cur_pos) {
-    };
-    int64_t cur_seed = verify((void*)mem->BUFFER[cur_pos].arr);
+  printf("Starting at %lu\n", cur_pos);
+  while (1) {
+    while (mem-> current_position == cur_pos) {};
+    int64_t cur_seed = verify((void*)mem-> BUFFER[cur_pos].arr);
     if (cur_seed == -1) {
       printf("Error!");
       break;
     }
-    printf("Verified at %d with seed %d\n", cur_pos, cur_seed);
+    printf("Verified at %lu with seed %lu\n", cur_pos, cur_seed);
     cur_pos++;
     cur_pos %= 1000;
   }
--- cyclicWrite.c
+++ cyclicWrite.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include "cyclicBuf.h"
+
 #include "gen.h"
 
 int main() {
@@ -16,26 +17,24 @@ int main() {
     return 1;
   }
 
-  int res;
-  res = ftruncate(shmFd, sizeof(struct cyclic_buf));
-  if (res == -1) {
+  if (ftruncate(shmFd, sizeof(struct cyclic_buffer)) == -1) {
     perror("Can't truncate file");
-    return res;
+    return -1;
   }
 
-  struct cyclic_buf* mem = mmap(NULL, sizeof(struct cyclic_buf), PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0);
+  struct cyclic_buffer* mem = mmap(NULL, sizeof(struct cyclic_buffer), PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0);
   if (mem == NULL) {
     perror("Can't mmap");
     return -1;
   }
 
-  mem->pos = 0;
+  mem->current_position = 0;
   uint32_t seed = 0;
   while (true) {
-    generate((void*)mem->BUFFER[mem->pos].arr, seed);
+    generate((void*)mem-> BUFFER[mem->current_position].arr, seed);
     seed++;
-    mem->pos++;
-    mem->pos %= 1000;
+    mem->current_position++;
+    mem->current_position %= 1000;
   }
 
   return 0;
